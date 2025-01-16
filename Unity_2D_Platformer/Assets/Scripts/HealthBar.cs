77bff1e9598c337037fcedf742cf36b294a7239a
Jaemin <wjm9932@gmail.com>
2025-01-16 17:12:15 +0900

    .

--- Unity_2D_Platformer/Assets/Scripts/HealthBar.cs
+++ Unity_2D_Platformer/Assets/Scripts/HealthBar.cs
@@ -8,7 +8,7 @@ public class HealthBar : MonoBehaviour
     [SerializeField] private Slider easeBar;
     [SerializeField] private Slider hpBar;
 
-    private float lerpSpeed = 3f;
+    private float lerpSpeed = 2f;
     public void UpdateHealthBar(float health, float maxHealth)
     {
         hpBar.value = health / maxHealth;
